30dd05127e9d48649aac12f1174a9ec5ff327e1f
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-09 23:45:07 +0300

    logo added

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+const Logo = () => {
+  return (
+    <div className='-mt-40'>
+      <img src={logo} alt='' />
+    </div>
+  );
+};
+
+export default Logo;
