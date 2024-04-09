e4c4b892a1b4fdc9e02e974bcfea9cc0def54081
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-09 23:44:36 +0300

    selectTip functin created

---
+++ src/components/SelectTip.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SelectTip = ({ setPercentage }) => {
+  const handleSelectTip = e => {
+    setPercentage(Number(e.target.value));
+  };
+  return (
+    <div>
+      <h2>Select Tip %</h2>
+      <div className='grid grid-cols-3 gap-4 max-w-96'>
+        <button onClick={handleSelectTip} value={5} className='bg-VeryDarkCyan text-white hover:bg-primary'>
+          5%
+        </button>
+        <button onClick={handleSelectTip} value={10} className='bg-VeryDarkCyan text-white hover:bg-primary'>
+          10%
+        </button>
+        <button onClick={handleSelectTip} value={15} className='bg-VeryDarkCyan text-white hover:bg-primary'>
+          15%
+        </button>
+        <button onClick={handleSelectTip} value={25} className='bg-VeryDarkCyan text-white hover:bg-primary'>
+          25%
+        </button>
+        <button onClick={handleSelectTip} value={50} className='bg-VeryDarkCyan text-white hover:bg-primary'>
+          50%
+        </button>
+        <button value={'custom'} className='bg-LightGrayishCyan text-white hover:bg-primary'>
+          Custom
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectTip;
