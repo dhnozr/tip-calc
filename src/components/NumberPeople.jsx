408aeb0f24237b76c7404cf1afde2e72758ec5f4
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-10 09:18:32 +0300

    label changed

--- src/components/NumberPeople.jsx
+++ src/components/NumberPeople.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const NumberPeople = ({ people, setPeople }) => {
   return (
     <div>
-      <label htmlFor='people'>Number of People</label>
+      <label className='text-DarkGrayishCyan' htmlFor='people'>
+        Number of People
+      </label>
       <input
         name='people'
         id='people'
         type='number'
-        className='h-7 border-2 bg-VeryLightGrayishCyan outline-LightGrayishCyan text-VeryDarkCyan'
+        className='h-10 p-1 border-2 w-full bg-VeryLightGrayishCyan outline-LightGrayishCyan text-VeryDarkCyan'
         placeholder='How many people'
         value={people}
         onChange={e => setPeople(e.target.value)}
